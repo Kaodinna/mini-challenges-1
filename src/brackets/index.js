884566c225269e493bc7d9f1c99e204f66d86c50
Kaodinna <chijiokenwoye64@gmail.com>
2022-09-12 21:58:48 +0100

    Assignment

--- src/brackets/index.js
+++ src/brackets/index.js
@@ -4,6 +4,25 @@
  * @param {string} str The string of brackets.
  * @returns {"valid" | "invalid"} Whether or not the string is valid.
  */
-function isValid(str) {}
+function isValid(str) {
+        const strArr = str.split('');
+        let counter = 0;
+        for (let i = 0, len = strArr.length; i < len; i++) {
+          if (strArr[i] === "(" || strArr[i] === "[" || strArr[i] === "{") {
+             counter++;
+          }else if (strArr[i] === ")" || strArr[i] === "]" || strArr[i] === "}") {
+             counter--;
+          };
+          if (counter < 0) {
+             return "invalid";
+          };
+        };
+        if (counter === 0) {
+          return "valid";
+        };
+        return "invalid" 
+}
+
+
 
 module.exports = isValid;
--- src/transpose/index.js
+++ src/transpose/index.js
@@ -4,6 +4,10 @@
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
-function transpose(array) {}
+function transpose(array) {
+    output = array[0].map((_, colIndex) => array.map(row => row[colIndex]));
+    var array = [];
+      return(output);
+}
 
 module.exports = transpose;
